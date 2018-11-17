bc6e6e428204ad9ee6ef7a504536d5091df27f98
Christian Liebel <christian.liebel@thinktecture.com>
2018-11-17 14:24:24 +0100

    fix(sw): skip waiting phase

--- sw.js
+++ sw.js
@@ -1,3 +1,5 @@
+self.addEventListener('install', () => self.skipWaiting());
+
 self.addEventListener('push', event => {
     const notification = event.data.json();
     self.registration.showNotification(notification.title, notification);
